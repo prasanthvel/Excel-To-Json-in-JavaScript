0b238b373b51657e99faa297942c23faa242bdf2
Prasanthvel <prasanthvel@gmail.com>
2017-08-09 13:03:03 +0530

    Update jsonToExcelConverter.js

--- jsonToExcelConverter.js
+++ jsonToExcelConverter.js
@@ -8,12 +8,12 @@
 var fs = require("fs");
 var json2xls = require('json2xls');
 
-var data = fs.readFileSync("test.json").toString(); // read the file and store the content
+var data = fs.readFileSync("jsonFile").toString(); // read the file and store the content
 //console.log(data);
 content = JSON.parse(data); // parse the file content into json
 var xls = json2xls(content); 
 
-fs.writeFileSync('Book1.xlsx', xls, 'binary');
+fs.writeFileSync('ExcelSheet', xls, 'binary');
 
 //console.log("Excel Sheet has written");
  
